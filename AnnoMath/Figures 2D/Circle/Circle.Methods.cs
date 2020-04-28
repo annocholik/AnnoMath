b54adef60ff539cb533e7ca958806fa7ba65de16
Paweł Sołtysik <po2po@poczta.fm>
2020-04-28 23:22:38 +0200

    Add class Circle

---
+++ AnnoMath/Figures 2D/Circle/Circle.Methods.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+/// <summary>
+/// AnnoMath library namespace with Figures 2D
+/// </summary>
+namespace AnnoMath.Figures2D
+{
+	/// <summary>
+    /// 2D Circle
+    /// </summary>
+    public partial class Circle
+    {
+        /// <summary>
+        /// Calculate Circle field
+        /// </summary>
+        /// <returns>Circle field</returns>
+        public float Field()
+        {
+            return (float)Math.PI * this.radius * this.radius;
+        }
+
+        /// <summary>
+        /// Calculate Circle circumference
+        /// </summary>
+        /// <returns>Circle circumference</returns>
+        public float Circumference()
+        {
+            return 2f * (float)Math.PI * this.radius;
+        }
+
+        /// <summary>
+        /// Calculate Circle diameter
+        /// </summary>
+        /// <returns>Circle diameter</returns>
+        public float Diameter()
+        {
+            return 2f * this.radius;
+        }
+    }
+}
